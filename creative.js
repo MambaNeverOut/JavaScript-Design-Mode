32bef870a9af0c6e3a355d03060877fac7f67926
Irelia <yaokai729@outlook.com>
2022-09-20 23:13:18 +0800

    工厂模式和抽象工厂模式

--- creative.js
+++ creative.js
@@ -1,18 +1,11 @@
-// 构造器
+// 构造器模式
 function User(name, age, career) {
   this.name = name;
   this.age = age;
   this.career = career;
 }
 
-// 工厂模式
-function User(name, age, career, work) {
-  this.name = name;
-  this.age = age;
-  this.career = career;
-  this.work = work;
-}
-
+// 工厂模式 1.0
 function Factory(name, age, career) {
   let work;
   switch (career) {
@@ -29,5 +22,92 @@ function Factory(name, age, career) {
       // ...
 
       return new User(name, age, career, work);
+  }
+}
+
+// 工厂模式 2.0
+// 需求：造出一台智能手机
+// 1. 一台手机通常由软件和硬件两部分构成
+class MobilePhoneFactory {
+  // 提供操作系统的接口
+  createOS() {
+    throw new Error("抽象工厂方法不允许直接调用，你需要将我重写！");
+  }
+  // 提供硬件的接口
+  createHardWare() {
+    throw new Error("抽象工厂方法不允许直接调用，你需要将我重写！");
+  }
+}
+// 2. 每一台手机都可以拥有自己的软件系统，并且硬件配置也是不同的
+// 具体工厂继承自抽象工厂
+class FakeStarFactory extends MobilePhoneFactory {
+    createOS() {
+        // 提供安卓系统实例
+        return new AndroidOS()
+    }
+    createHardWare() {
+        // 提供高通硬件实例
+        return new QualcommHardWare()
+    }
+}
+// 3.1 软件系统内部也有不同的调用硬件的方法
+// 定义操作系统这类产品的抽象产品类
+class OS {
+    controlHardWare() {
+        throw new Error('抽象产品方法不允许直接调用，你需要将我重写！');
+    }
+}
+
+// 定义具体操作系统的具体产品类
+class AndroidOS extends OS {
+    controlHardWare() {
+        console.log('我会用安卓的方式去操作硬件')
+    }
+}
+
+class AppleOS extends OS {
+    controlHardWare() {
+        console.log('我会用🍎的方式去操作硬件')
+    }
+}
+
+// 定义手机硬件这类产品的抽象产品类
+class HardWare {
+    // 手机硬件的共性方法，这里提取了“根据命令运转”这个共性
+    operateByOrder() {
+        throw new Error('抽象产品方法不允许直接调用，你需要将我重写！');
+    }
+}
+// 3.2 硬件系统也有不同的芯片，不同的运行方式
+// 定义具体硬件的具体产品类
+class QualcommHardWare extends HardWare {
+    operateByOrder() {
+        console.log('我会用高通的方式去运转')
+    }
+}
+
+class MiWare extends HardWare {
+    operateByOrder() {
+        console.log('我会用小米的方式去运转')
+    }
+}
+// 4. 最后，我们只需要把软件和硬件都运行起来，保证软件和硬件各司其职就可以了
+// 这是我的手机
+const myPhone = new FakeStarFactory()
+// 让它拥有操作系统
+const myOS = myPhone.createOS()
+// 让它拥有硬件
+const myHardWare = myPhone.createHardWare()
+// 启动操作系统(输出‘我会用安卓的方式去操作硬件’)
+myOS.controlHardWare()
+// 唤醒硬件(输出‘我会用高通的方式去运转’)
+myHardWare.operateByOrder()
+// 5. 假如有一天我们生成的手机过气了，我们想要开发一台新的手机，我们只需要新建一个手机种类，并添加它的软件和硬件即可
+class newStarFactory extends MobilePhoneFactory {
+  createOS() {
+    // 操作系统实现代码
+  }
+  createHardWare() {
+    // 硬件实现代码
   }
 }
